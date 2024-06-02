d9f708e4a39a0b09bcb990a6144faf1780a8dd89
Nand <nandvadera25@gmail.com>
2024-06-02 15:24:11 +0530

    added logo to categories

--- database/migrations/2024_05_25_020703_create_categories_table.php
+++ database/migrations/2024_05_25_020703_create_categories_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('logo')->nullable();
             $table->enum('status', ['Active', 'Inactive'])->default('Active');
             $table->foreignId('created_by')->nullable()->constrained('users');
             $table->foreignId('updated_by')->nullable()->constrained('users');

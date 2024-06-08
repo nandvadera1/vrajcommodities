06dfd936ad47d9a1c97f28e0f39f2c393fc8b44a
Nand <nandvadera25@gmail.com>
2024-06-08 23:33:31 +0530

    bug fix

--- app/Http/Controllers/Api/v1/User/ItemController.php
+++ app/Http/Controllers/Api/v1/User/ItemController.php
@@ -130,10 +130,10 @@ class ItemController extends Controller
                               $item->image = asset('image/' . $item->image);
                          }
                          if(!empty($item->pdf)){
-                              $item->image = asset('pdf/' . $item->pdf);
+                              $item->pdf = asset('pdf/' . $item->pdf);
                          }
                          if(!empty($item->excel)){
-                              $item->image = asset('excel/' . $item->excel);
+                              $item->excel = asset('excel/' . $item->excel);
                          }
                     }
                }
